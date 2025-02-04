b8c3fe078e390757e70d284c46934094987acbb1
Eshaan Joshi <eshaanj@gmail.com>
2025-02-04 12:38:10 -0600

    vercel page speed insights

--- packages/www/src/app/layout.tsx
+++ packages/www/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@/components/Navbar"
 
@@ -36,6 +37,7 @@ export default function RootLayout({
           {children}
         </main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

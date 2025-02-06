c5c359fb06b8e4a5394a5b8b1d2519ff3eb23d2a
Eshaan Joshi <eshaanj@gmail.com>
2025-02-06 15:34:15 -0600

    added shadcn toast component

--- packages/www/src/lib/utils.ts
+++ packages/www/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertImageToBase64(image: File): Promise<string | undefined> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(image)
+    reader.onload = () => {
+      resolve(reader.result as string)
+    }
+    reader.onerror = (error) => {
+      reject(error)
+    }
+  })
+}
+
+export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
